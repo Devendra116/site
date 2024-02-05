d23427c24faf2ac69c0049d4b2b06e5eea8c6c64
Devendra Chauhan <76091413+Devendra116@users.noreply.github.com>
2024-02-05 05:36:35 +0000

    update hero button

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -86,10 +86,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
+      href="#about"
+      // target="_blank"
       rel="noreferrer">
-      Check out my course!
+      About Me
     </a>
   );
 
